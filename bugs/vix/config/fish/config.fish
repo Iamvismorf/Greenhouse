b3261865fd444aa0e454e1312dd0f614311e8c0b
vismorf <96656664+Iamvismorf@users.noreply.github.com.>
2025-10-03 00:26:04 +0200

    add shell wrapper to yazi

--- bugs/vix/config/fish/config.fish
+++ bugs/vix/config/fish/config.fish
@@ -1,4 +1,12 @@
 if status is-interactive
+   function y
+      set tmp (mktemp -t "yazi-cwd.XXXXXX")
+      yazi $argv --cwd-file="$tmp"
+      if read -z cwd < "$tmp"; and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+         builtin cd -- "$cwd"
+      end
+      rm -f -- "$tmp"
+   end
    fzf --fish | source
    set fish_greeting 
    set -g fish_key_bindings fish_vi_key_bindings
