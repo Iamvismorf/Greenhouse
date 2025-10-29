00af0a5e4c3b2f30341a2b9c21c908c0f7643194
vismorf <96656664+Iamvismorf@users.noreply.github.com.>
2025-10-29 22:34:01 +0100

    .

--- hosts/Daffodil/configuration.nix
+++ hosts/Daffodil/configuration.nix
@@ -18,16 +18,17 @@
   vsmrf.enable = false;
   environment.systemPackages = [
     pkgs.networkmanagerapplet
+    #todo: rebuild and see if these are needed
     # pkgs.kdePackages.kservice
     # pkgs.kdePackages.baloo
     # pkgs.kdePackages.baloo-widgets
     # pkgs.kdePackages.dolphin-plugins
   ];
 
-  # environment.variables = {
-  #   EDITOR = "nvim";
-  #   VISUAL = "nvim";
-  # };
+  environment.variables = {
+    EDITOR = "nvim";
+    VISUAL = "nvim";
+  };
 
   system.stateVersion = "24.11";
 }
