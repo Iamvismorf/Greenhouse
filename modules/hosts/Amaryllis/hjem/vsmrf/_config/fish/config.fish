b65d21f524cf8369f34536e1ed613dd1d4d3ec09
vismorf <96656664+Iamvismorf@users.noreply.github.com.>
2026-03-16 07:39:45 +0100

    remove fzf tab cmp

--- modules/hosts/Amaryllis/hjem/vsmrf/_config/fish/config.fish
+++ modules/hosts/Amaryllis/hjem/vsmrf/_config/fish/config.fish
@@ -14,7 +14,7 @@ if status is-interactive
    fish_vi_key_bindings --no-erase insert
    fish_default_key_bindings -M insert
 
-   bind -M insert \t 'fzf-complete'
+   # bind -M insert \t 'fzf-complete'
    bind -M insert ctrl-space accept-autosuggestion
    bind -M visual l forward-char-passive
    bind l forward-char-passive
@@ -52,48 +52,48 @@ end
 #direnv hook fish | source
 
 
-function fzf-complete -d 'fzf completion and print selection back to commandline'
-	set -l cmd (commandline -co) (commandline -ct)
-	switch $cmd[1]
-		case env sudo
-			for i in (seq 2 (count $cmd))
-				switch $cmd[$i]
-					case '-*'
-					case '*=*'
-					case '*'
-						set cmd $cmd[$i..-1]
-						break
-				end
-			end
-	end
-	set cmd (string join -- ' ' $cmd)
-
-	set -l complist (complete -C$cmd)
-	set -l result
-	string join -- \n $complist | sort | eval (__fzfcmd) -m --select-1 --exit-0 --header '(commandline)' | cut -f1 | while read -l r; set result $result $r; end
-
-	set prefix (string sub -s 1 -l 1 -- (commandline -t))
-	for i in (seq (count $result))
-		set -l r $result[$i]
-		switch $prefix
-			case "'"
-				commandline -t -- (string escape -- $r)
-			case '"'
-				if string match '*"*' -- $r >/dev/null
-					commandline -t --  (string escape -- $r)
-				else
-					commandline -t -- '"'$r'"'
-				end
-			case '~'
-				commandline -t -- (string sub -s 2 (string escape -n -- $r))
-			case '*'
-				commandline -t -- (string escape -n -- $r)
-		end
-		[ $i -lt (count $result) ]; and commandline -i ' '
-	end
-
-	commandline -f repaint
-end
+# function fzf-complete -d 'fzf completion and print selection back to commandline'
+# 	set -l cmd (commandline -co) (commandline -ct)
+# 	switch $cmd[1]
+# 		case env sudo
+# 			for i in (seq 2 (count $cmd))
+# 				switch $cmd[$i]
+# 					case '-*'
+# 					case '*=*'
+# 					case '*'
+# 						set cmd $cmd[$i..-1]
+# 						break
+# 				end
+# 			end
+# 	end
+# 	set cmd (string join -- ' ' $cmd)
+#
+# 	set -l complist (complete -C$cmd)
+# 	set -l result
+# 	string join -- \n $complist | sort | eval (__fzfcmd) -m --select-1 --exit-0 --header '(commandline)' | cut -f1 | while read -l r; set result $result $r; end
+#
+# 	set prefix (string sub -s 1 -l 1 -- (commandline -t))
+# 	for i in (seq (count $result))
+# 		set -l r $result[$i]
+# 		switch $prefix
+# 			case "'"
+# 				commandline -t -- (string escape -- $r)
+# 			case '"'
+# 				if string match '*"*' -- $r >/dev/null
+# 					commandline -t --  (string escape -- $r)
+# 				else
+# 					commandline -t -- '"'$r'"'
+# 				end
+# 			case '~'
+# 				commandline -t -- (string sub -s 2 (string escape -n -- $r))
+# 			case '*'
+# 				commandline -t -- (string escape -n -- $r)
+# 		end
+# 		[ $i -lt (count $result) ]; and commandline -i ' '
+# 	end
+#
+# 	commandline -f repaint
+# end
 
 
 function last_history_item
