29564a494790b5526e291d7263d5e83a93bd43ac
vismorf <96656664+Iamvismorf@users.noreply.github.com.>
2026-03-22 10:15:47 +0100

    fish: remove fzf integration

--- modules/hosts/Daffodil/hjem/wawffle/_config/fish/config.fish
+++ modules/hosts/Daffodil/hjem/wawffle/_config/fish/config.fish
@@ -8,7 +8,6 @@ if status is-interactive
       end
       rm -f -- "$tmp"
    end
-   fzf --fish | source
    set fish_greeting 
    set -g fish_key_bindings fish_vi_key_bindings
    fish_vi_key_bindings --no-erase insert
