1be89aca04eb5b389a25931830f905c3cb84a37e
vismorf <96656664+Iamvismorf@users.noreply.github.com.>
2026-06-18 23:20:23 +0200

    firefox

--- modules/hosts/Amaryllis/hjem/vsmrf/_config/firefox/user.js
+++ modules/hosts/Amaryllis/hjem/vsmrf/_config/firefox/user.js
@@ -6,4 +6,8 @@ user_pref("toolkit.legacyUserProfileCustomizations.stylesheets" , true);
 user_pref("extensions.pocket.enabled" , false);
 user_pref("browser.toolbars.bookmarks.visibility" , "never");
 user_pref("ui.key.menuAccessKeyFocuses" , false);
-
+user_pref("browser.search.defaultenginename", "DuckDuckGo");
+user_pref("browser.search.selectedEngine", "DuckDuckGo");
+user_pref("sidebar.verticalTabs", true);
+user_pref("sidebar.visibility", "always-show");
+user_pref("widget.use-xdg-desktop-portal.file-picker", 1);
